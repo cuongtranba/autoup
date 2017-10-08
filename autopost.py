3a0d054ce21f6355d69a1af78372e2271bf851ae
DESKTOP-LU0C24T\cuong <bacuongtr@gmail.com>
2017-10-08 12:13:08 +0700

    no message

---
+++ autopost.py
@@ -0,0 +1,50 @@
+import json
+import threading
+import time
+import datetime
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from cx_Freeze import setup, Executable
+
+def load_config_file():
+    with open('config.json') as data_file:
+        return json.load(data_file)
+
+def up_thread(urls, timeLoop, count, driver):
+    for i in range(count):
+        print(f'up lan:{i+1}/{count}')
+        for link in urls:
+            driver.get(link + '/up')
+            print(link + '  ----> xong')
+        currentDateTime = datetime.datetime.now()
+        nextTimeToUp = datetime.datetime.now() + datetime.timedelta(minutes=timeLoop)
+        print(f'up tiep luc:{nextTimeToUp}')
+        time.sleep(timeLoop * 60)
+def login(username, password, loginPage):
+    driver = webdriver.Chrome('chromedriver.exe')
+    driver.get(loginPage)
+    usernameBox = driver.find_element_by_id('ctrl_pageLogin_login')
+    passwordBox = driver.find_element_by_id('ctrl_pageLogin_password')
+    submitBox = driver.find_element_by_css_selector('#pageLogin > dl.ctrlUnit.submitUnit > dd > input')
+
+    usernameBox.send_keys(username)
+    passwordBox.send_keys(password)
+    submitBox.submit()
+    return driver
+def main():
+    data = load_config_file()
+    accounts = data["accounts"]
+    upTime = data["count"]
+    loginPage = data["loginPage"]
+    count = data["count"]
+    threads = []
+    for item in accounts:
+        urls = item["linkup"]
+        username = item["username"]
+        password = item["password"]
+        upInEvery = item["upInEvery"]
+        driver = login(username,password,loginPage)
+        up_thread(urls, upInEvery, count, driver)
+        driver.quit()
+if __name__ == '__main__':
+    main()
